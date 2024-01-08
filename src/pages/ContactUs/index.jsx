14e24171b300dddb8c4a6c77281604ce1568556c
Pedro Martins <pedro.mourao.martins@example.com>
2024-01-08 23:30:58 +0000

    Add Contact Us

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -1,9 +1,19 @@
+import { Layout, Card, Button,Form,Input } from 'antd';
+const { Content } = Layout;
+const { Meta } = Card;
 export default function ContactUs() {
     return (
-        <>
-            <section id="Z79zTyCNzVdNbfq2" style="position:relative;overflow:hidden;display:grid;align-items:center;grid-template-columns:auto 100rem auto;z-index:0;">
-               
-            </section>
-        </>
+        <Content>
+          <Card  >
+            <h2>Send  your question:</h2>
+          <Form  name="contact" netlify>
+  <Form.Item label="Name"> <Input type="text" name="name" /></Form.Item>
+  <Form.Item label="Email"> <Input type="email" name="email" /></Form.Item>
+  <Form.Item label="Message"> <Input type="textarea" name="message" /></Form.Item>
+    <Button style={{ color:'white',backgroundColor:'blue'}} type="submit">Send</Button>
+</Form>
+          </Card>
+          
+        </Content>
     );
 }
