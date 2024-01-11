2eb3926efcad677b1614402957a126006dc43754
Pedro Martins <pedro.mourao.martins@example.com>
2024-01-11 23:03:21 +0000

    add

--- src/components/Projects/Lessons/LessonDetails.jsx
+++ src/components/Projects/Lessons/LessonDetails.jsx
@@ -5,7 +5,37 @@ const LessonDetails = ({ Lesson,onClose }) => {
   if (!Lesson) {
     return <p>No Lesson selected</p>;
   }
-
+  const downloadBlogPostData = (BlogPost) => {
+    let blogPostData = `Type: ${BlogPost.BlogPost_type}\n`;
+    blogPostData += `Description: ${BlogPost.description}\n`;
+    blogPostData += `Content: ${BlogPost.content}\n\n`;
+  
+    blogPostData += `Chapters:\n`;
+    BlogPost.chapters?.forEach((chapter, index) => {
+      blogPostData += `  Chapter ${index + 1}: ${chapter?.name}\n`;
+      chapter.sections?.forEach((section, sIndex) => {
+        blogPostData += `    Section ${sIndex + 1}: ${section?.title}\n`;
+        blogPostData += `    ${section?.content}\n`;
+      });
+    });
+  
+    // Create a Blob from the string content
+    const blob = new Blob([blogPostData], { type: 'text/plain' });
+  
+    // Create a link element, set the download attribute with a filename
+    const link = document.createElement('a');
+    link.download = `${BlogPost.title}.txt`;
+  
+    // Create a URL for the blob and set as link's href
+    link.href = window.URL.createObjectURL(blob);
+  
+    // Append to the document and trigger the download
+    document.body.appendChild(link);
+    link.click();
+  
+    // Clean up and remove the link
+    link.parentNode.removeChild(link);
+  };
   return (
     <Drawer
     title={Lesson?.title || 'Lesson Details'}
@@ -15,6 +45,9 @@ const LessonDetails = ({ Lesson,onClose }) => {
       visible={true}
       extra={
         <Space>
+           <Button type="primary" onClick={() => downloadBlogPostData(BlogPost)}>
+    Download Blog Post 
+  </Button>
         <Button type="primary"onClick={onClose}>
           Close
         </Button>
@@ -29,11 +62,11 @@ const LessonDetails = ({ Lesson,onClose }) => {
         <h3>Chapters:</h3>
         {Lesson?.chapters?.map((chapter, index) => (
           <div key={index}>
-            <h4>{chapter.name}</h4>
+            <h4>{chapter?.name}</h4>
             {chapter?.sections?.map((section, sIndex) => (
               <div key={sIndex}>
-                   <h3>Section:{section.title} </h3>
-             <p> {section.content}</p></div>
+                   <h3>Section:{section?.title} </h3>
+             <p> {section?.content}</p></div>
             ))}
           </div>
         ))}
