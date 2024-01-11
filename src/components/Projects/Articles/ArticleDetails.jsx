bd5fe51211e4f2596d51328f8545ea54b9616293
Pedro Martins <pedro.mourao.martins@example.com>
2024-01-11 22:35:33 +0000

    add articles

--- src/components/Projects/Articles/ArticleDetails.jsx
+++ src/components/Projects/Articles/ArticleDetails.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import { Card,Drawer,Space,Button } from 'antd';
-
+import { DownloadOutlined } from '@ant-design/icons';
 const ArticleDetails = ({ Article,onClose }) => {
   if (!Article) {
     return <p>No Article selected</p>;
   }
-
+  const downloadArticleData = (Article) => {
+    let articleData = `Type: ${Article.Article_type}\n`;
+    articleData += `Description: ${Article.description}\n`;
+    articleData += `Content: ${Article.content}\n\n`;
+  
+    articleData += `Sections:\n`;
+    Article.Sections?.forEach((Section, index) => {
+      articleData += `  Section ${index + 1}: ${Section.name}\n`;
+      Section.sections?.forEach((section, sIndex) => {
+        articleData += `    Subsection ${sIndex + 1}: ${section.title}\n`;
+        articleData += `    ${section.content}\n`;
+      });
+    });
+  
+    // Create a Blob from the string content
+    const blob = new Blob([articleData], { type: 'text/plain' });
+  
+    // Create a link element, set the download attribute with a filename
+    const link = document.createElement('a');
+    link.download = `${Article.title}.txt`;
+  
+    // Create a URL for the blob and set as link's href
+    link.href = window.URL.createObjectURL(blob);
+  
+    // Append to the document and trigger the download
+    document.body.appendChild(link);
+    link.click();
+  
+    // Clean up and remove the link
+    link.parentNode.removeChild(link);
+  };
   return (
     <Drawer
     title={Article?.title || 'Article Details'}
@@ -15,6 +45,9 @@ const ArticleDetails = ({ Article,onClose }) => {
       visible={true}
       extra={
         <Space>
+           <Button icon={<DownloadOutlined />}  onClick={() => downloadArticleData(Article)}>
+    Download Article 
+  </Button>
         <Button type="primary"onClick={onClose}>
           Close
         </Button>
@@ -29,11 +62,11 @@ const ArticleDetails = ({ Article,onClose }) => {
         <h3>Sections:</h3>
         {Article?.Sections?.map((Section, index) => (
           <div key={index}>
-            <h4>{Section.name}</h4>
-            {Section?.sections.map((section, sIndex) => (
+            <h4>{Section?.name}</h4>
+            {Section?.sections?.map((section, sIndex) => (
               <div key={sIndex}>
-                   <h3>Section:{section.title} </h3>
-             <p> {section.content}</p></div>
+                   <h3>Section:{section?.title} </h3>
+             <p> {section?.content}</p></div>
             ))}
           </div>
         ))}
--- src/components/Projects/Articles/ArticleEdit.jsx
+++ src/components/Projects/Articles/ArticleEdit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button,Drawer,Card, Select, Space } from 'antd';
+import { Form, Input,Row,Col,Collapse, Button,Drawer,Card, Select, Space } from 'antd';
 import {  CloseOutlined } from '@ant-design/icons';
 const { Option } = Select;
 const { TextArea } = Input;
@@ -28,107 +28,165 @@ const ArticleEdit = ({ Article, onClose, onSave }) => {
       </Space>
       }
     >
-      <Form
-        form={form}
-        layout="vertical"
-        initialValues={{
-          title: Article.title,
-          description: Article.description,
-          Article_type: Article.Article_type,
-          content: Article.content,
-        }}
-        onFinish={handleSave}
-      >
-       <Form.Item
-          name="title"
-          label="Title"
-          rules={[{ required: true, message: 'Please input the title!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          name="description"
-          label="Description"
-          rules={[{ required: true, message: 'Please input the description!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          name="Article_type"
-          label="Article Type"
-          rules={[{ required: true, message: 'Please select the Article type!' }]}
-        >
-          <Select>
-            <Option value="fiction">Fiction</Option>
-            <Option value="science">Science</Option>
-            <Option value="other">Other</Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item
-          name="content"
-          label="Content"
-          rules={[{ required: true, message: 'Please input the content!' }]}
-        >
-          <TextArea rows={4} />
-        </Form.Item>
-        <Form.List name="Sections">
-    {(fields, { add, remove }) => (
-      <div style={{ display: 'flex', rowGap: 16, flexDirection: 'column' }}>
-        {fields?.map((field) => (
-          <Card
-            size="small"
-            title={`Section ${field.name + 1}`}
-            key={field.key}
-            extra={
-              <CloseOutlined
-                onClick={() => {
-                  remove(field.name);
+   <Form layout="vertical" form={form} id="ArticleForm" onFinish={handleSave} >
+        {/* Form fields here */}
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="title"
+              label="Title"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter Article title',
+                },
+              ]}
+            >
+              <Input placeholder="Please enter Article title" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="description"
+              label="Description"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter description',
+                },
+              ]}
+            >
+              <Input
+                style={{
+                  width: '100%',
                 }}
+                placeholder="Please enter description"
               />
-            }
-          >
-            <Form.Item label="Section Title" name={[field.name, 'name']}>
-              <Input />
             </Form.Item>
-            {/* Nest Form.List */}
-            <Form.Item label="Sections">
-              <Form.List name={[field.name, 'sections']}>
-                {(subFields, subOpt) => (
-                  <div style={{  rowGap: 16 }}>
-                    {subFields?.map((subField) => (
-                      <Space key={subField.key}>
-                        <Form.Item noStyle name={[subField.name, 'title']}>
-                          <Input placeholder="Title" />
-                        </Form.Item>
-                        <Form.Item noStyle name={[subField.name, 'content']}>
-                          <TextArea placeholder="Content" />
-                        </Form.Item>
-                        <CloseOutlined
-                          onClick={() => {
-                            subOpt.remove(subField.name);
-                          }}
-                        />
-                      </Space>
-                    ))}
-                    <Button type="dashed" onClick={() => subOpt.add()} block>
-                      + Add Sub Section
-                    </Button>
-                  </div>
-                )}
-              </Form.List>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              name="Article_type"
+              label="Article Type"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please select an Article type',
+                },
+              ]}
+            >
+              <Select placeholder="Please select an Article type">
+                <Option value="fiction">Fiction</Option>
+                <Option value="science">Science</Option>
+                <Option value="other">Other</Option>
+              </Select>
             </Form.Item>
-          </Card>
-        ))}
+          </Col>
+        </Row>
 
-        <Button type="dashed" onClick={() => add()} block>
-          + Add Section
-        </Button>
-      </div>
-    )}
-  </Form.List>  
+        <Row gutter={16}>
+          <Col span={24}>
+            <Collapse
+              size="small"
+              items={[
+                {
+
+                  label: `Outline Content`,
+                  children:
+                    <Form.Item
+                      name="content"
+                      label="Content"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'please enter  content',
+                        },
+                      ]}
+                    >
+                      <Input.TextArea rows={4} placeholder="please enter content " />
+                    </Form.Item>,
+                },
+              ]}
+            />
+          </Col>
+        </Row>
+        <Form.List name="Sections">
+          {(fields, { add, remove }) => (
+            <div style={{ display: 'flex', rowGap: 16, flexDirection: 'column' }}>
+              {fields.map((field) => (
+
+                <Collapse
+                  size="small"
+                  items={[
+                    {
+                      key: field.key,
+                      label: `Section ${field.name + 1}`,
+                      children:
+                        <Card
+                          size="small"
+                          title={`Section ${field.name + 1}`}
+                          key={field.key}
+                          extra={
+                            <CloseOutlined
+                              onClick={() => {
+                                remove(field.name);
+                              }}
+                            />
+                          }
+                        >
+                          <Form.Item label="Section Title" name={[field.name, 'name']}>
+                            <Input />
+                          </Form.Item>
+                          {/* Nest Form.List */}
+                          <Form.Item label="SubSections">
+                            <Form.List name={[field.name, 'sections']}>
+                              {(subFields, subOpt) => (
+                                <div style={{ rowGap: 16 }}>
+                                  {subFields.map((subField) => (
+                                    <Collapse
+                                      size="small"
+                                      items={[
+                                        {
+                                          key: subField.key,
+                                          label: 'SubSection:' + subField.key,
+                                          children:
+                                            <Card title={'Section:' + subField.key} extra={<CloseOutlined
+                                              onClick={() => {
+                                                subOpt.remove(subField.name);
+                                              }}
+                                            />} key={subField.key}>
+                                              <Form.Item noStyle name={[subField.name, 'title']}>
+                                                <Input placeholder="Title" />
+                                              </Form.Item>
+                                              <Form.Item noStyle name={[subField.name, 'content']}>
+                                                <Card>   <TextArea placeholder="Content" /></Card>
+                                              </Form.Item>
+                                            </Card>,
+                                        },
+                                      ]}
+                                    />
+                                  ))}
+                                  <Button type="dashed" onClick={() => subOpt.add()} block>
+                                    + Add Sub Section
+                                  </Button>
+                                </div>
+                              )}
+                            </Form.List>
+                          </Form.Item>
+                        </Card>,
+                    },
+                  ]}
+                />
+              ))}
+
+              <Button type="dashed" onClick={() => add()} block>
+                + Add Section
+              </Button>
+            </div>
+          )}
+        </Form.List>
       </Form>
       
     </Drawer>
